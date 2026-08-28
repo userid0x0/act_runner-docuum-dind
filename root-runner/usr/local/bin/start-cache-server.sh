607c806e4850bcdb5c2ed27e369224fa94140f39
Alexander Merkle <Alexander.Merkle@lauterbach.com>
2026-08-28 08:39:45 +0200

    chore: remove obsolete start-cache-server.sh

--- root-runner/usr/local/bin/start-cache-server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cat <<EOF > /tmp/config.yaml
-cache:
-  external_secret: ${GITEA_CACHE_SECRET}
-EOF
-gitea-runner cache-server -c /tmp/config.yaml -s ${GITEA_CACHE_HOSTNAME} -p ${GITEA_CACHE_PORT} -d /data/cache
